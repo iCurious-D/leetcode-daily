cf846bf0a4f276601c929f6af62b2b2e6c278548
iCurious D <ginading@126.com>
2026-06-26 11:25:01 +0800

    day52

---
+++ leetcode_daily/26-06-26.py
@@ -0,0 +1,59 @@
+""" 3739. 统计主要元素子数组数目 II    困难
+给你一个整数数组 nums 和一个整数 target。返回数组 nums 中满足 target 是 主要元素 的 子数组 的数目。
+一个子数组的 主要元素 是指该元素在该子数组中出现的次数 严格大于 其长度的 一半 。
+子数组 是数组中的一段连续且 非空 的元素序列。
+
+示例 1:输入: nums = [1,2,2,3], target = 2;  输出: 5
+解释:以 target = 2 为主要元素的子数组有:
+nums[1..1] = [2]    nums[2..2] = [2]    nums[1..2] = [2,2]  nums[0..2] = [1,2,2]    nums[1..3] = [2,2,3]
+因此共有 5 个这样的子数组。
+
+示例 2:输入: nums = [1,1,1,1], target = 1;  输出: 10
+解释:所有 10 个子数组都以 1 为主要元素。
+
+示例 3:输入: nums = [1,2,3], target = 4;    输出: 0
+解释:target = 4 完全没有出现在 nums 中。因此，不可能有任何以 4 为主要元素的子数组。故答案为 0。
+
+提示:
+1 <= nums.length <= 10^5
+1 <= nums[i] <= 10^9
+1 <= target <= 10^9
+=========================================================================================
+
+题解路径：. / leetcode_daily_stories / 26-06-26.md
+
+"""
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
+        # sl = SortedList([0])
+        # ans = s = 0
+        # for num in nums:
+        #     s += 1 if num == target else -1
+        #     ans += sl.bisect_left(s)
+        #     sl.add(s)
+        # return ans
+
+        cnt = defaultdict(int)
+        cnt[0] = 1
+        ans = s = f = 0
+        for num in nums:
+            if num == target:
+                f += cnt[s]
+                s += 1
+            else:
+                s -= 1
+                f -= cnt[s]
+            ans += f
+            cnt[s] += 1
+
+        return ans
+
+
+if __name__ == '__main__':
+    print(Solution().countMajoritySubarrays(nums=[1, 2, 2, 3], target=2))
+    print(Solution().countMajoritySubarrays(nums=[1, 1, 1, 1], target=1))
+    print(Solution().countMajoritySubarrays(nums=[1, 2, 3], target=4))

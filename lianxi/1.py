0954c7cd8a24c49547802063a13a5223388d00b8
iCurious D <ginading@126.com>
2026-08-02 16:07:03 +0800

    day88

--- lianxi/1.py
+++ lianxi/1.py
@@ -151,5 +151,45 @@ def rearrangeString(s: str, x: str, y: str) -> str:
     return ans
 
 
+""" Q1. 按下时间最长的按钮   简单  3 分
+给你一个二维数组 events，表示孩子在键盘上按下一系列按钮触发的按钮事件。
+每个 events[i] = [indexi, timei] 表示在时间 timei 时，按下了下标为 indexi 的按钮。
+数组按照 time 的递增顺序排序。
+按下一个按钮所需的时间是连续两次按钮按下的时间差。按下第一个按钮所需的时间就是其时间戳。
+返回按下时间 最长 的按钮的 index。如果有多个按钮的按下时间相同，则返回 index 最小的按钮。
+ 
+示例 1：输入： events = [[1,2],[2,5],[3,9],[1,15]];   输出： 1
+解释：下标为 1 的按钮在时间 2 被按下。
+下标为 2 的按钮在时间 5 被按下，因此按下时间为 5 - 2 = 3。
+下标为 3 的按钮在时间 9 被按下，因此按下时间为 9 - 5 = 4。
+下标为 1 的按钮再次在时间 15 被按下，因此按下时间为 15 - 9 = 6。
+最终，下标为 1 的按钮按下时间最长，为 6。
+
+提示：
+1 <= events.length <= 1000
+events[i] == [indexi, timei]
+1 <= indexi, timei <= 105
+输入保证数组 events 按照 timei 的递增顺序排序。
+"""
+def buttonWithLongestTime(events: List[List[int]]) -> int:
+    max_duration = -1
+    result_idx = float('inf')
+
+    prev_time = 0
+    for i, (idx, time) in enumerate(events):
+        # 计算当前事件的持续时间
+        if i == 0:
+            duration = time
+        else:
+            duration = time - prev_time
+
+        # 更新结果:找持续时间最长的按钮
+        if duration > max_duration or (duration == max_duration and idx < result_idx):
+            max_duration = duration
+            result_idx = idx
+
+        prev_time = time
+
+    return result_idx
 
 

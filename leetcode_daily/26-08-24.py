3f0280d5ce74580630540afda02104aba26aeafa
iCurious D <ginading@126.com>
2026-08-24 20:33:57 +0800

    day109

---
+++ leetcode_daily/26-08-24.py
@@ -0,0 +1,80 @@
+""" 1872. 石子游戏 VIII     困难
+Alice 和 Bob 玩一个游戏，两人轮流操作， Alice 先手 。
+总共有 n 个石子排成一行。轮到某个玩家的回合时，如果石子的数目 大于 1 ，他将执行以下操作：
+选择一个整数 x > 1 ，并且 移除 最左边的 x 个石子。
+将 移除 的石子价值之 和 累加到该玩家的分数中。
+将一个 新的石子 放在最左边，且新石子的值为被移除石子值之和。
+当只剩下 一个 石子时，游戏结束。
+Alice 和 Bob 的 分数之差 为 (Alice 的分数 - Bob 的分数) 。 Alice 的目标是 最大化 分数差，Bob 的目标是 最小化 分数差。
+
+给你一个长度为 n 的整数数组 stones ，其中 stones[i] 是 从左边起 第 i 个石子的价值。
+请你返回在双方都采用 最优 策略的情况下，Alice 和 Bob 的 分数之差 。
+
+示例 1：输入：stones = [-1,2,-3,4,-5];    输出：5
+解释：- Alice 移除最左边的 4 个石子，得分增加 (-1) + 2 + (-3) + 4 = 2 ，并且将一个价值为 2 的石子放在最左边。stones = [2,-5] 。
+- Bob 移除最左边的 2 个石子，得分增加 2 + (-5) = -3 ，并且将一个价值为 -3 的石子放在最左边。stones = [-3] 。
+两者分数之差为 2 - (-3) = 5 。
+示例 2：输入：stones = [7,-6,5,10,5,-2,-6];   输出：13
+解释：- Alice 移除所有石子，得分增加 7 + (-6) + 5 + 10 + 5 + (-2) + (-6) = 13 ，并且将一个价值为 13 的石子放在最左边。stones = [13] 。
+两者分数之差为 13 - 0 = 13 。
+示例 3：输入：stones = [-10,-12];     输出：-22
+解释：- Alice 只有一种操作，就是移除所有石子。得分增加 (-10) + (-12) = -22 ，并且将一个价值为 -22 的石子放在最左边。stones = [-22] 。
+两者分数之差为 (-22) - 0 = -22 。
+
+提示：
+n == stones.length
+2 <= n <= 10^5
+-104 <= stones[i] <= 10^4
+
+"""
+from functools import cache
+from itertools import accumulate
+from typing import List
+
+def stoneGameVIII(stones: List[int]) -> int:
+    n = len(stones)
+    f = s = sum(stones)
+    for i in range(n-2, 0, -1):
+        s -= stones[i+1]
+        f = max(s - f, f)
+    return f
+
+    # pre_sum = list(accumulate(stones))
+    #
+    # f = [0] * n
+    # f[-1] = pre_sum[-1]
+    # for i in range(n-2, 0, -1):
+    #     f[i] = max(pre_sum[i]-f[i+1], f[i+1])
+    # return f[1]
+
+    # @cache
+    # def dfs(i: int) -> int:
+    #     if i == n-1:
+    #         return pre_sum[-1]
+    #     return max(pre_sum[i]-dfs(i+1), dfs(i+1))
+    #
+    # return dfs(1)
+
+
+if __name__ == '__main__':
+    print(stoneGameVIII([7,-6,5,10,5,-2,-6]))
+    print(stoneGameVIII([-1,2,-3,4,-5]))
+    print(stoneGameVIII([-10,-12]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

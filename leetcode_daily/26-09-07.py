3b86888adf7f2a12cc1101f20b308a518194b2a6
iCurious D <ginading@126.com>
2026-09-07 13:50:47 +0800

    day123

---
+++ leetcode_daily/26-09-07.py
@@ -0,0 +1,51 @@
+""" 940. 不同的子序列 II      困难
+给定一个字符串 s，计算 s 的 不同非空子序列 的个数。因为结果可能很大，所以返回答案需要对 10^9 + 7 取余 。
+字符串的 子序列 是经由原字符串删除一些（也可能不删除）字符但不改变剩余字符相对位置的一个新字符串。
+例如，"ace" 是 "abcde" 的一个子序列，但 "aec" 不是。
+
+示例 1：输入：s = "abc";      输出：7
+解释：7 个不同的子序列分别是 "a", "b", "c", "ab", "ac", "bc", 以及 "abc"。
+示例 2：输入：s = "aba";      输出：6
+解释：6 个不同的子序列分别是 "a", "b", "ab", "ba", "aa" 以及 "aba"。
+示例 3：输入：s = "aaa";      输出：3
+解释：3 个不同的子序列分别是 "a", "aa" 以及 "aaa"。
+
+提示：
+1 <= s.length <= 2000
+s 仅由小写英文字母组成
+
+"""
+
+def distinctSubseqII(s: str) -> int:
+    mod = 10**9 + 7
+    f = [0] * 26
+    # for c in s:
+    #     f[ord(c) - ord('a')] = (1 + sum(f)) % mod
+    # return sum(f) % mod
+
+    total = 0
+    for c in s:
+        c = ord(c) - ord('a')
+        others = total - f[c]
+        f[c] = 1 + total
+        total = (f[c] + others) % mod
+
+    return total
+
+
+if __name__ == '__main__':
+    print(distinctSubseqII("abc"))
+    print(distinctSubseqII("aba"))
+    print(distinctSubseqII("aaa"))
+
+
+
+
+
+
+
+
+
+
+
+
